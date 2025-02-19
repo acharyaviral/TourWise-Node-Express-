2d6b596ca056b20e92629a9fb744629da3dba287
viralacharya <acharyaviral03@gmail.com>
2025-02-19 13:47:30 +0530

    feat: Create tours & users routes

--- 4- project/app.js
+++ 4- project/app.js
@@ -1,54 +1,31 @@
-const fs = require("node:fs");
-
 const express = require("express");
+const morgan = require("morgan");
+
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
+
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
-// app.get("/", (req, res) => {
-// 	res.status(200).json({ message: "Hello World!" });
-// });
-
-// app.post("/", (req, res) => {
-// 	res.status(200).json({ message: "Hello World!" });
-// });
-
-const tours = JSON.parse(
-	fs.readFileSync(`${__dirname}/dev-data/data/tours.json`),
-);
-
-app.get("/api/v1/tours", (req, res) => {
-	res.status(200).json({
-		status: "success",
-		results: tours.length,
-		data: { tours },
-	});
+app.use((req, res, next) => {
+	console.log("Hello from the middleware 👋");
+	next();
 });
 
-app.post("/api/v1/tours", (req, res) => {
-	// console.log(req.body);
-
-	const newId = tours[tours.length - 1].id + 1;
-	const newTour = Object.assign({ id: newId }, req.body);
-	tours.push(newTour);
-
-	fs.writeFile(
-		`${__dirname}/dev-data/data/tours.json`,
-		JSON.stringify(tours),
-		(err) => {
-			res.status(201).json({
-				status: "success",
-				data: {
-					tour: newTour,
-				},
-			});
-			console.log("The file was saved!");
-		},
-	);
+app.use((req, res, next) => {
+	req.requestTime = new Date().toISOString();
+	next();
 });
 
-app.listen(PORT, () => {
-	console.log(`App listening on port ${PORT}`);
-});
+// 3) ROUTES
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+
+module.exports = app;
