d5b820d7fd3934973cfe7c3548863544e50da032
viralacharya <acharyaviral03@gmail.com>
2025-02-19 22:10:45 +0530

    feat: setup MongoDB with Mongoose, connect to Atlas, add tour routing, controller & model

--- 4- project/server.js
+++ 4- project/server.js
@@ -1,7 +1,18 @@
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const app = require("./app");
 
 dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+	"<PASSWORD>",
+	process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+	.connect(DB)
+	.then(() => console.log("DB connection successful!"))
+	.catch((err) => console.error("MongoDB Connection Error:", err));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
