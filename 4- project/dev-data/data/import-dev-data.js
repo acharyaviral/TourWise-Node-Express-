72e78b2fd60dda1e719f2a34e032ebe485e2717d
viralacharya <acharyaviral03@gmail.com>
2025-02-20 12:17:03 +0530

    feat: create tour import script & tour schema

---
+++ 4- project/dev-data/data/import-dev-data.js
@@ -0,0 +1,52 @@
+const fs = require("node:fs");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Tour = require("./../../models/tourModel");
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+	"<PASSWORD>",
+	process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB).then(() => {
+	console.log("DB connection successful!");
+});
+
+const tours = JSON.parse(
+	fs.readFileSync(`${__dirname}/tours-simple.json`, "utf-8"),
+);
+
+// Import data
+const importData = async () => {
+	try {
+		await Tour.create(tours); // Removed extra JSON.parse()
+		console.log("Data successfully loaded!");
+		process.exit(); // Ensure script exits
+	} catch (err) {
+		console.error(err);
+		process.exit(1); // Exit with error
+	}
+};
+
+// DELETE All data from DB
+const deleteData = async () => {
+	try {
+		await Tour.deleteMany();
+		console.log("Data successfully deleted!");
+		process.exit(); // Ensure script exits
+	} catch (err) {
+		console.error(err);
+		process.exit(1); // Exit with error
+	}
+};
+
+if (process.argv[2] === "--import") {
+	importData();
+} else if (process.argv[2] === "--delete") {
+	deleteData();
+} else {
+	console.log("Invalid command. Use --import or --delete.");
+	process.exit(1);
+}
