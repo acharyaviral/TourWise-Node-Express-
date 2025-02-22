57c05d8e8413ce2f7b0cdcafe61b136c11653483
viralacharya <acharyaviral03@gmail.com>
2025-02-22 22:20:28 +0530

    feat: Implement user aurthorization middleware

--- 4- project/models/userModel.js
+++ 4- project/models/userModel.js
@@ -17,6 +17,11 @@ const userSchema = new mongoose.Schema({
 		validate: [validator.isEmail, "Please provide a valid email"],
 	},
 	photo: String,
+	role: {
+		type: String,
+		enum: ["user", "guide", "lead-guide", "admin"],
+		default: "user",
+	},
 	password: {
 		type: String,
 		required: [true, "Please provide a password"],
@@ -35,11 +40,6 @@ const userSchema = new mongoose.Schema({
 		},
 	},
 	passwordChangedAt: Date,
-	role: {
-		type: String,
-		enum: ["user", "guide", "lead-guide", "admin"],
-		default: "user",
-	},
 });
 
 userSchema.pre("save", async function (next) {
