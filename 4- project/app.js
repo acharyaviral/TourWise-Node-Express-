bcf0b8914422b0b4e3e88bde3c235101e3fac6fe
viralacharya <acharyaviral03@gmail.com>
2025-02-23 21:52:35 +0530

    feat: - Implement updating the current user - Implement deleting the current user - Implement JWT authentication using cookies - Add rate limiting for API requests - Implement data sanitization for inputs - Prevent parameter pollution attacks - Enhance security with proper HTTP headers

--- 4- project/app.js
+++ 4- project/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+//const xss = require("xss");
+const hpp = require("hpp");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -7,15 +12,50 @@ const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
 const app = express();
+// 1) Global MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
 
-// 1) MIDDLEWARES
+// Development logging
 if (process.env.NODE_ENV === "development") {
 	app.use(morgan("dev"));
 }
 
-app.use(express.json());
+//  Rate limiting
+const limiter = rateLimit({
+	max: 100,
+	windowsMs: 60 * 60 * 1000,
+	message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+// NEED  to impliment  xss!!!!!!!!!!!
+
+//Prevent parameter pollution
+app.use(
+	hpp({
+		whitelist: [
+			"duration",
+			"ratingsQuantity",
+			"ratingsAverage",
+			"maxGroupSize",
+			"difficulty",
+			"price",
+		],
+	}),
+);
+
+// serve static files
 app.use(express.static(`${__dirname}/public`));
 
+// 2) Test MIDDLEWARE
 app.use((req, res, next) => {
 	req.requestTime = new Date().toISOString();
 
