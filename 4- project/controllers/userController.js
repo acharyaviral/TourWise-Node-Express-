fa80773388721357912723a69776bd52c679c4fa
viralacharya <acharyaviral03@gmail.com>
2025-02-28 15:02:06 +0530

    feat: -Impliment Tour image upload

--- 4- project/controllers/userController.js
+++ 4- project/controllers/userController.js
@@ -31,7 +31,7 @@ const upload = multer({
 
 exports.uploadUserPhoto = upload.single("photo");
 
-exports.resizeUserPhoto = (req, res, next) => {
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
 	if (!req.file) return next();
 
 	req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
@@ -43,7 +43,7 @@ exports.resizeUserPhoto = (req, res, next) => {
 		.toFile(`public/img/users/${req.file.filename}`);
 
 	next();
-};
+});
 
 const filterObj = (obj, ...allowedFields) => {
 	const newObj = {};
